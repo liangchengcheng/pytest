44407354268f75165d374c58b9190e04e4aa648a
liangchengcheng <1038127753@qq.com>
2018-04-18 09:07:29 +0800

    add simple code
    add simple code

---
+++ test/isprint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+a = 20
+b = 20
+
+if (a is b):
+    print "1 - a 和 b 有相同的标识"
+else:
+    print "1 - a 和 b 没有相同的标识"
+
+if (a is not b):
+    print "2 - a 和 b 没有相同的标识"
+else:
+    print "2 - a 和 b 有相同的标识"
+
+# 修改变量 b 的值
+b = 30
+if (a is b):
+    print "3 - a 和 b 有相同的标识"
+else:
+    print "3 - a 和 b 没有相同的标识"
+
+if (a is not b):
+    print "4 - a 和 b 没有相同的标识"
+else:
+    print "4 - a 和 b 有相同的标识"
+
+#is 用于判断两个变量引用对象是否为同一个，
+
+#  == 用于判断引用变量的值是否相等。
+
